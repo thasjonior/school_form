1bb2e1116b4980e2800913d12238abebd238714f
mickamwamba <kimollomick17@gmail.com>
2020-02-20 13:23:34 +0300

    rename to schoolform

---
+++ schoolform/database/migrations/2020_02_20_064631_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Fname');
+            $table->string('Sname');
+            $table->date('BirthDate');
+            $table->integer('Age');
+            $table->string('Gender');
+            $table->string('PhysicalAddress');
+            $table->string('PostalAddress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
